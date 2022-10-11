f0c4708ed3b3f7827953e1ab42659c1a5220aa34
suraj-mane <sun.mane1100@gmail.com>
2022-10-11 17:36:19 +0530

    adding title

--- portfolio-minimal/src/pages/index.js
+++ portfolio-minimal/src/pages/index.js
@@ -13,11 +13,11 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="suraj-mane" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         {/* <ArticlesSection sectionId="articles" heading="Apps / Websites I've Built" sources={['Medium']} /> */}
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
+        {/* <AboutSection sectionId="about" heading="About Portfolio Minimal" /> */}
         <InterestsSection sectionId="details" heading="Skills & Technologies" />
         <ProjectsSection sectionId="features" heading="Apps / Websites I've Built" />
         <ContactSection sectionId="github" heading="Reach Out to Me!" />
