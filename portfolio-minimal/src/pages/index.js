83c988d6593bee406ab0af650aa45ee1ec23ebfa
suraj-mane <sun.mane1100@gmail.com>
2022-10-11 14:10:17 +0530

    add resume

---
+++ portfolio-minimal/src/pages/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  AboutSection,
+  ArticlesSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Page useSplashScreenAnimation>
+        <HeroSection sectionId="hero" />
+        {/* <ArticlesSection sectionId="articles" heading="Apps / Websites I've Built" sources={['Medium']} /> */}
+        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
+        <InterestsSection sectionId="details" heading="Skills & Technologies" />
+        <ProjectsSection sectionId="features" heading="Apps / Websites I've Built" />
+        <ContactSection sectionId="github" heading="Reach Out to Me!" />
+      </Page>
+    </>
+  );
+}
